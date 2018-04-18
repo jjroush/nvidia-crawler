56e5f2bbf1d63b2e890481df7fb268977752f3d4
Jacob Roush <jacobroushmail@gmail.com>
2018-04-17 23:32:15 -0500

    kinda works?

--- server.js
+++ server.js
@@ -2,13 +2,33 @@ const express = require('express');
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
-const app     = express();
+const app = express();
 
 app.get('/scrape', function(req, res){
 
   url = "https://www.nvidia.com/en-us/geforce/products/10series/geforce-gtx-1080-ti/";
 
+  request(url, function(error, response, html){
+    if(!error){
+        const $ = cheerio.load(html);
+        
+        let sale;
+
+        $('.js-product-item').eq(0).filter(function(){
+
+
+            const data = $(this);
+
+            sale = data.children().eq(0).addClass();
+
+            console.log(sale);
+            
+
+        })
+    }
+  })
 })
+
 
 app.listen('8081')
 
